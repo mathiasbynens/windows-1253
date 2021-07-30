59586a6c29b892a5ea7ffa6b64ed45ca97e11190
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:24:44 +0200

    Release v1.0.2

--- windows-1253.js
+++ windows-1253.js
@@ -1,4 +1,4 @@
-/*! https://mths.be/windows-1253 v1.0.1 by @mathias | MIT license */
+/*! https://mths.be/windows-1253 v1.0.2 by @mathias | MIT license */
 ;(function() {
 
 	var object = {};
@@ -104,7 +104,7 @@
 			'windows-1253',
 			'x-cp1253'
 		],
-		version: '1.0.1',
+		version: '1.0.2',
 	};
 
 	module.exports = windows1253;
